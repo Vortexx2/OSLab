85eb1c3c9c0c5a7206ada50e62289e12c135694e
Vortexx2 <janmayshuklay@gmail.com>
2022-04-25 14:16:10 +0530

    Finished Assignment11

--- Assignment11/bankers.py
+++ Assignment11/bankers.py
@@ -1,4 +1,5 @@
 from typing import Final, List
+
 from utility import *
 
 NUM_RESOURCES: Final = 4
@@ -8,9 +9,17 @@ NUM_PROCESSES: Final = 5
 if __name__ == "__main__":
 
   processes, available = example_processes()
-
-
-
+  gantt = Gantt(NUM_PROCESSES)
 
   for i in range(NUM_PROCESSES):
-    new_proc = Process(i, NUM_RESOURCES)
+    did_proc_exec = False
+
+    for proc in processes:
+      if proc.can_execute(available):
+        available = proc.execute(available)
+
+        gantt.add(proc, available)
+
+        did_proc_exec = True
+
+  print(gantt)
--- Assignment11/utility.py
+++ Assignment11/utility.py
@@ -3,7 +3,7 @@ from typing import List, Optional, Tuple
 
 # ---------------- Below all of the utility methods have been defined ----------------------
 
-def example_processes() -> Tuple(List["Process"], List[int]):
+def example_processes() -> Tuple[List["Process"], List[int]]:
   """
     Method to return (Processes, Initial_Available_Resources)
   """
@@ -20,9 +20,11 @@ def example_processes() -> Tuple(List["Process"], List[int]):
 # ---------------- Below all of the utility classes have been defined ----------------------
 
 class Process:
-  def __init__(self, pid: int, num_resources: int, allocated: List[int] = [], max_need: List[int] = []):
+  def __init__(self, pid: int, num_resources: int,
+               allocated: Optional[List[int]] = None, max_need: Optional[List[int]] = None):
     self.pid = pid
     self.num_resources = num_resources
+    self.executed = False
 
     if allocated and max_need:
       assert (len(allocated) == self.num_resources and len(
@@ -33,3 +35,54 @@ class Process:
     else:
       self.allocated = [0] * self.num_resources
       self.max_need = [0] * self.num_resources
+
+  @property
+  def need(self) -> List[int]:
+    """ The need array for a process which is (max_need - allocated) """
+    return [(max_res - alloc) for (max_res, alloc) in zip(self.max_need, self.allocated)]
+
+  def can_execute(self, available: List[int]) -> bool:
+    """
+      Returns True if process can execute - respective available resources > need resources\n
+      Else returns False
+    """
+    if self.executed:
+      return False
+
+    for avail, need in zip(available, self.need):
+      if avail < need:
+        return False
+
+    return True
+
+  def execute(self, available: List[int]) -> List[int]:
+    """
+      Executes the current process. Should only be done after checking if `can_execute()`.\n
+      Sets `self.executed` flag to `True` and returns new list of available resources.
+    """
+    self.executed = True
+    return [(avail + alloc) for avail, alloc in zip(available, self.allocated)]
+
+
+class Gantt:
+
+  def __init__(self, num_procs: int):
+    self.num_procs = num_procs
+    self.arr = []
+
+  def add(self, proc: Process, available: List[int]) -> None:
+    self.arr.append((proc, available))
+
+  def __str__(self) -> str:
+
+    if len(self.arr) != self.num_procs:
+      return "There is no viable sequence\n"
+
+    res = ""
+    for proc, available in self.arr:
+      res += f"Process {proc.pid} has been executed. New available resource list is {available}\n"
+
+    return res
+
+  def __len__(self) -> int:
+    return len(self.arr)
